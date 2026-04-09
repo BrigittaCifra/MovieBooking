cee5e69a0fcacd456ff8d24e6f74e32460a09d4b
natconley <natalieannaconley@gmail.com>
2026-04-09 14:37:46 +0200

    validations

--- MovieBookingApp/src/services/getMovies.js
+++ MovieBookingApp/src/services/getMovies.js
@@ -1,5 +1,8 @@
-// imports mockdata
+
+// importerar mockdata
 import { titleData, showtimesData } from "./mockData.js";
+
+// importera API nycklar
 const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 if (!TMDB_API_KEY || !OMDB_API_KEY) {
@@ -7,6 +10,9 @@ if (!TMDB_API_KEY || !OMDB_API_KEY) {
 }
 
 export async function getMovie(movie) {
+     // matchar ihop showtimesData med movie id
+     // innan try för att nås från try och catch
+        const movieShowtimes = showtimesData.filter((showtime) => showtime.movieId === movie.id);
     try {
         const omdbResponse = await fetch(`https://www.omdbapi.com/?t=${movie.title}&apikey=${OMDB_API_KEY}`);
         const omdbData = await omdbResponse.json();
@@ -14,31 +20,45 @@ export async function getMovie(movie) {
         const tmdbResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie.title}&api_key=${TMDB_API_KEY}`);
         const tmdbData = await tmdbResponse.json();
 
-        // TMDB lämnar en array av resultat, följande hämtar första resultatet
+        // TMDB lämnar en array av resultat, följande hämtar första resultatet av två bildtyper
 
         // portrait till cards & alla bilder som är högre än de är breda
+        // null om path inte finns
         const posterPath = tmdbData.results[0].poster_path;
-        const portrait = `https://image.tmdb.org/t/p/w342${posterPath}`;
+        const portrait = posterPath ? `https://image.tmdb.org/t/p/w342${posterPath}` : null;
 
         // backdrop till hero/details & alla bilder som är bredare än de är höga
+        // null om path inte finns
         const backdropPath = tmdbData.results[0].backdrop_path;
-        const hero = `https://image.tmdb.org/t/p/w1280${backdropPath}`;
+        const hero = backdropPath ? `https://image.tmdb.org/t/p/w1280${backdropPath}` : null;
 
-        // matchar ihop showtimesData med movie id
-        const movieShowtimes = showtimesData.filter((showtime) => showtime.movieId === movie.id);
-
-        // objektet som skapas
+        // objektet som skapas (med validering)
         return {
             id: movie.id,
             title: movie.title,
             newRelease: movie.newRelease,
             comingSoon: movie.comingSoon,
-            description: omdbData.Plot,
-            rating: omdbData.imdbRating,
-            runtime: omdbData.Runtime,
-            genre: omdbData.Genre,
-            portraitImg: portrait,
-            heroImg: hero,
+
+            description: omdbData.Plot !== "N/A"
+            ? omdbData.Plot
+            : "No description available",
+
+            rating: omdbData.imdbRating !== "N/A"
+            ? omdbData.imdbRating
+            : "No rating",
+
+            runtime: omdbData.Runtime !== "N/A"
+            ? omdbData.Runtime
+            : "Runtime unknown",
+
+            genre: omdbData.Genre !== "N/A"
+            ? omdbData.Genre 
+            : "Unknown",
+
+            portraitImg: portrait || "/images/placeholderPortrait.png",
+
+            heroImg: hero || "/images/placeholderHero.png",
+
             showtimes: movieShowtimes
         }
     } catch (error) {
@@ -53,12 +73,14 @@ export async function getMovie(movie) {
             rating: "-",
             runtime: "-",
             genre: "Unknown",
-            portraitImg: "",
-            heroImg:""
+            portraitImg: "/images/placeholderPortrait.png",
+            heroImg:"/images/placeholderHero.png",
+            showtimes: movieShowtimes
         };
     }
 }
 
+// använder getMovie och anropar alla
 export async function getMovies() {
     const movies = await Promise.all(
         titleData.map((movie) => getMovie(movie))
