f71677f9a323b5a30955addaad935e6655abf597
rakorenato <renato.rako@hotmail.com>
2026-04-13 23:11:21 +0200

    feat: add HeroCarousel component with TMDB API

--- MovieBookingApp/src/App.jsx
+++ MovieBookingApp/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from "./components/Footer/Footer";
+import HeroCarousel from "./components/HeroCarousel/HeroCarousel";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <HeroCarousel />
       <Footer />
     </>
   )
