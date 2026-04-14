0246ee6a0c9bf6b6fcef90085eaa9234cfa0e807
natconley <natalieannaconley@gmail.com>
2026-04-14 15:17:56 +0200

    hero video logic in details page, ui in detailsHero

--- MovieBookingApp/src/components/Footer/Details/DetailsHero.jsx
+++ MovieBookingApp/src/components/Footer/Details/DetailsHero.jsx
@@ -1,10 +1,28 @@
 
-// <DetailsHero movie={movie} />
 
- export default function DetailsHero({ movie }) {
+
+
+
+
+export default function DetailsHero({ movie, trailerUrl, TrailerIsLoading }) {
+
     return (
-        <div>
-            <img src={movie.heroImg} alt={movie.title} />
+        <div className="detailsHero">
+            {!TrailerIsLoading && trailerUrl ? (
+                <iframe
+                    src={trailerUrl}
+                    title={movie.title}
+                    allowFullScreen
+                />
+            ) : (
+                <img src={movie.heroImg} alt={movie.title} />
+            )}
+            <div className="heroContent">
+                <h2>{movie.title}</h2>
+                <p>{movie.description}</p>
+                {/* genre, runtime, rated, skådespelare */}
+            </div>
         </div>
     )
 }
+
---
+++ MovieBookingApp/src/components/Footer/Details/DetailsPage.jsx
@@ -0,0 +1,37 @@
+
+// DetailsHero tar trailer från DetailsPage
+// <DetailsHero movie={movie} trailerUrl={trailer} TrailerIsLoading={TrailerIsLoading} />
+import { getTrailer } from "../../services/getTrailer.js";
+import { useEffect, useState } from "react";
+import { DetailsHero } from "./DetailsHero";
+
+export default function DetailsPage({ movie }) {
+    const [trailerUrl, setTrailerUrl] = useState(null);
+    const [trailerIsLoading, setTrailerIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchTrailer() {
+            setTrailerIsLoading(true);
+            try {
+            const trailer = await getTrailer(movieId);
+            setTrailerUrl(trailer);
+            setTrailerIsLoading(false);
+            } catch (err) {
+                console.error("Error fetching trailer");
+                return null;
+            }
+        }
+        fetchTrailer();
+    }, [movie]);
+
+    return (
+        <div>
+            <DetailsHero
+            movie={movie}
+            trailerUrl={trailerUrl}
+            trailerIsLoading={trailerIsLoading}
+            />
+        </div>
+    )
+
+}
